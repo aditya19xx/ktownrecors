5ee0aface7c18fce088b8743e25e32931fdfb9ae
aditya19xx <turjaadityadas@gmail.com>
2021-11-22 17:16:25 +0600

    artist section updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css';
 const Banner = () => {
     return (
         <div> 
-        <h1 style={{color:'white', fontFamily: 'Righteous', fontSize:'100px'}}>KTwonRecords</h1>
+        <h1 style={{color:'white', fontFamily: 'Righteous', fontSize:'80px'}}><span style={{color:'red'}}>K</span>Twon<span style={{color:'red'}}>Records</span></h1>
         </div>
     );
 };
